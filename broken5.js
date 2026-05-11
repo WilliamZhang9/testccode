57b82b6eeb03545fdbae1a83e0c05ffe66048c74
William Zhang <william.zhang@live.ca>
2026-05-11 09:59:30 -0400

    Apply QA-approved Gemini fixes

--- broken5.js
+++ broken5.js
@@ -1,29 +1,28 @@
-let firstItem = "Apple"; 
-
 function processCart(cartItems) {
     let totalAmnt = 0;
-    let taxRate = 0.05;
+    let taxRate = 0.05; 
 
-    const validCategories = ['fruit', 'vegetable', 'meat'];
+    const validCategories = ['fruit', 'vegetable', 'meat']; 
 
-    if (taxRate === 0) {
+    if (taxRate === 0) { 
         console.log("Tax free!");
     }
 
-    for (let i = 0; i < cartItems.length; i++) {
-        
-        if (cartItems[i].price > 100) {
+    for (let i = 0; i < cartItems.length; i++) { 
+
+        if (cartItems[i].price > 100) { 
             console.log("Expensive item!");
         }
 
         totalAmnt += cartItems[i].price;
     }
 
-    console.log("Subtotal is: " + totalAmnt);
-    let finalTotal = totalAmnt * (1 + taxRate);
-    console.log("Total is: " + finalTotal);
+    console.log("Subtotal is: " + totalAmnt); 
 
-    return finalTotal;
+    let totalWithTax = totalAmnt * (1 + taxRate); 
+    console.log("Total is: " + totalWithTax); 
+
+    return totalAmnt;
 }
 
-processCart([{name: "Item1", price: 10}, {name: "Item2", price: 15}]);
+processCart([{name: "Apple", price: 50}, {name: "Banana", price: 20}]);
