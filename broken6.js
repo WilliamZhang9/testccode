19f1a31de5c78e8c8550f0fbafaab16dbd5eb037
William Zhang <william.zhang@live.ca>
2026-05-11 11:05:07 -0400

    Apply QA-approved Gemini fixes

---
+++ broken6.js
@@ -0,0 +1,24 @@
+let firstTemp = 100;
+
+function convertCelsiusToFahrenheit(celsius) {
+    let fahrenheit = 0;
+    
+    if (celsius === 0) {
+        console.log("Freezing point!");
+    }
+
+    fahrenheit = (celsius * 9/5) + 32;
+
+    if (fahrenheit > 212) {
+        console.log("Boiling point exceeded!");
+    }
+
+    return fahrenheit; 
+}
+
+let temps = [0, 10, 100];
+
+for (let i = 0; i < temps.length; i++) {
+    let res = convertCelsiusToFahrenheit(temps[i]);
+    console.log("Result: " + res);
+}
