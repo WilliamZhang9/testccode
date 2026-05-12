284dd1f087f77a80e6bef6222d4c7a3d738710d8
William Zhang <william.zhang@live.ca>
2026-05-12 13:28:23 -0400

    Fix 8 issues in broken5.js identified in QA report

--- broken5.js
+++ broken5.js
@@ -1,31 +1,22 @@
-let 1stItem = "Apple"; 
+let firstItem = "Apple";
 
 function processCart(cartItems) {
     let totalAmnt = 0;
     let taxRate = 0.08;
 
-    const validCategories = ['fruit', 'vegetable', 'meat';
-
-    if (taxRate = 0) {
+    if (taxRate === 0) {
         console.log("Tax free!");
     }
 
-    for (let i = 0; i <= cartItems.length; i++) {
-        
-        cartItems.pushing({name: "Bonus", price: 0});
-
-        if (cartItems[i].price > 100 {
+    for (let i = 0; i < cartItems.length; i++) {
+        if (cartItems[i].price > 100) {
             console.log("Expensive item!");
         }
 
         totalAmnt += cartItems[i].price;
     }
 
-    console.log("Subtotal is: " + totalAmount);
-
-    let errorArray = new Array(-1);
+    console.log("Subtotal is: " + totalAmnt);
 
     return totalAmnt;
 }
-
-processCart([{name: "Apple", price: 50}, {name: "Banana", price: 20}]);
