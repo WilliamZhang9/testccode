13a40a6e4dfc3319b10da1aea4da23e21c334f5d
William Zhang <william.zhang@live.ca>
2026-05-12 13:01:59 -0400

    Fix bugs identified in QA report: broken1.py, broken2.py, broken3.py

--- broken2.py
+++ broken2.py
@@ -1,17 +1,9 @@
 import json
 
-def load_config():
-    config = '{"debug": true, "port": 8080'  # ❌ missing closing }
+config_json = '{"port": 8080, "debug": true}'
+config = json.loads(config_json)
 
-    data = json.loads(config)
-    return data
+if config["debug"]:
+    print("Debug mode ON")
 
-def start_server():
-    config = load_config()
-
-    if config["debug"] == True:
-        print("Debug mode ON")
-
-    print("Running on port " + config["port"])  # ❌ int + str
-
-start_server()
+print("Running on port " + str(config["port"]))
