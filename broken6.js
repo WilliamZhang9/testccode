6a185a2f8b5dfd61a058b8213344a73f5dbbf2ab
William Zhang <william.zhang@live.ca>
2026-05-12 12:44:41 -0400

    Fix syntax, logic, and reference errors in broken6.js

--- broken6.js
+++ broken6.js
@@ -1,24 +1,17 @@
-let 1stTemp = 100;
+let firstTemp = 100;
 
 function convertCelsiusToFahrenheit(celsius) {
-    let fahrenheit = 0;
-    
-    if (celsius = 0) {
-        console.log("Freezing point!");
+    if (celsius === 0) {
+        console.log("Freezing point");
     }
-
-    fahrenheit = (celsius * 9/5) + 32;
-
-    if (fahrenheit > 212 {
-        console.log("Boiling point exceeded!");
+    let fahrenheit = (celsius * 9/5) + 32;
+    if (fahrenheit > 212) {
+        console.log("Boiling point");
     }
-
-    return farenheit; 
+    return fahrenheit;
 }
 
-let temps = [0, 10, 100];
-
-for (let i = 0; i <= temps.length; i++) {
-    let res = convertCelsiusToFahrenheit(temps[i]);
-    console.log("Result: " + res);
+let temps = [0, 20, 30];
+for (let i = 0; i < temps.length; i++) {
+    console.log(convertCelsiusToFahrenheit(temps[i]));
 }
