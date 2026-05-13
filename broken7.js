43678728b1d78021260bda7c98544beffa737d16
wandouuu <130308899+wandouuu@users.noreply.github.com>
2026-05-13 16:19:12 -0400

    add broken7.js

---
+++ broken7.js
@@ -0,0 +1,29 @@
+const 1stValue = 10; 
+
+let dataList = [1, 2, 3, 4; 
+
+function calculateStats(arr) {
+    let sum = 0;
+    
+    for (let i = 0; i <= arr.length; i++) { 
+        
+        if (arr[i] = 5) { 
+            
+            console.log("Found a 5!); 
+        }
+        sum += arr[i];
+    }
+
+    if (sum > 20 { 
+        console.log("Sum is large");
+    }
+
+    let average = sum / arr.legnth; 
+
+    return summ; 
+}
+
+let myString = "Metrics";
+myString.push(" Data"); 
+
+let result = calulateStats(dataList);
